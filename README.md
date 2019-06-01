bed97d0ba278c85f4330e2e0c8d9985c5c7c6b2c
Yaroslavik <yaroslav4167@mail.ru>
2019-06-01 13:26:30 +0200

    Release!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RuFix
+This is a Sponge plugin for auto-execute commands written on russian layout.
+Compatibility: SpongeAPI-7.1.0
---
+++ src/main/java/rufix/RuFix.java
@@ -0,0 +1,48 @@
+package rufix;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandMapping;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.event.Order;
+import org.spongepowered.api.event.command.SendCommandEvent;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.plugin.Plugin;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+@Plugin(id = "ruFix", name = "RuFix", version = "1.0", description = "ruFix")
+public class RuFix {
+    private List<String> alias = new ArrayList<>();
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        alias.clear();
+        for (CommandMapping commandMapping : Sponge.getCommandManager().getCommands()) {
+            alias.add(commandMapping.getPrimaryAlias());
+            alias.addAll(commandMapping.getAllAliases());
+        }
+    }
+
+    @Listener(order = Order.LAST)
+    public void onCommand(SendCommandEvent event) {
+        String tCom = convert(event.getCommand());
+        if(event.getCommand().equals(tCom))return;
+        CommandSource src = event.getCause().first(CommandSource.class).orElse(null);
+        if(src==null||!alias.contains(convert(event.getCommand())))return;
+        Sponge.getCommandManager().process(src, tCom + " " +convert(event.getArguments()));
+        event.setCancelled(true);
+    }
+
+    private String convert(String str){
+        String s = str;
+        String out = "`~@#$^&|qwertyuiop[]QWERTYUIOP{}asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?sS]}";
+        String in = "ёЁ\"№;:?/йцукенгшщзхъЙЦУКЕНГШЩЗХЪфывапролджэФЫВАПРОЛДЖЭячсмитьбю.ЯЧСМИТЬБЮ,іІїЇ";
+        for (int i = 0; i < in.length(); i++)
+            s = s.replace(in.charAt(i), out.charAt(i));
+        return s;
+    }
+}
+
